0343a913a9c0f3fcdd834e11194395c75bb15771
unknown <tstarciuc@EN62507.endava.net>
2015-07-24 10:08:20 +0300

    tatiana - US-31 Unit Testing

--- tests/js/server-data-test.js
+++ tests/js/server-data-test.js
@@ -13,18 +13,17 @@ QUnit.test("server-data-module.getServerData, Testing URL validation", function(
 QUnit.test("server-data-module.getServerData, Testing ajax call content result", function(assert) {
 	"use strict";
 	var params = {},
-		result = {};
+		result = {},
+		ajaxResult = THUNDERSTORM.modules.ajaxCallback();
+
 	assert.ok(1 === 1, "Passed.");
-	var ajaxResult = THUNDERSTORM.modules.ajaxCallback();
-
+	
 	params = {"url" : "http://localhost:3000/rest/articles/",
-				  "method" : "GET",
-				  "dataType" : "json",
-				  "callback" : ajaxResult.testFct, 
-				  "errCallback": ajaxResult.errorHandler};
+			  "callback" : ajaxResult.testFct, 
+			  "errCallback": ajaxResult.errorHandler};
 
-	THUNDERSTORM.serverDataInstance = new THUNDERSTORM.modules.serverDataModule();
-	result = THUNDERSTORM.serverDataInstance.getServerData(params);
+	result = THUNDERSTORM.modules.API.get(params);
+
 	setTimeout(function(){
 		assert.ok(result.responseJSON !== null, "Ajax call result is not null.");
 	}, 1000);
