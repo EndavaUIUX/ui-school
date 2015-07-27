f7485f679425f784eec2e2e0068a0d1d6121d069
Tatiana <tstarciuc@EN62507.endava.net>
2015-07-27 16:39:44 +0300

    update

--- assets/js/article-details.js
+++ assets/js/article-details.js
@@ -2,9 +2,9 @@
     'use strict';
     var utility = THUNDERSTORM.modules.utility;
     var articleIndex = window.location.href.substr(window.location.href.indexOf("#") + 1);
-    for(var i = 0, len = THUNDERSTORM.articleData.length; i < len; i++) {
-        if(THUNDERSTORM.articleData[i].id === articleIndex){
-            utility.populateArticleDetails(THUNDERSTORM.articleData[i], i);
+    for(var i = 0, len = THUNDERSTORM.modules.articles.data.length; i < len; i++) {
+        if(THUNDERSTORM.modules.articles.data[i].id === articleIndex){
+            utility.populateArticleDetails(THUNDERSTORM.modules.articles.data[i], i);
         }
     }
 }());
