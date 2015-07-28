def4f218890cdb5ee94b67c20f0cf58ff0a9458e
IuliaCojocaru <iulia.cojocaru@endava.com>
2015-07-28 15:23:23 +0300

    gallery added

--- assets/js/UI/article-content.js
+++ assets/js/UI/article-content.js
@@ -14,7 +14,10 @@
         articleUrlNumber = pageUrl.split("#")[1],
         article = THUNDERSTORM.modules.articles.data['articles'][articleUrlNumber],
         currentArticle = THUNDERSTORM.modules.utility.imageSourceGenerator(article),
-        viewMoreButton = $("<div></div>").html("View gallery").addClass("button__gallery");
+        viewMoreButton = $("<div></div>").html("View gallery").addClass("button__gallery"),
+        utility = THUNDERSTORM.modules.utility,
+        articleIndex = window.location.href.substr(window.location.href.indexOf("#") + 1),
+        articleContent = THUNDERSTORM.modules.articles.data['articles'][articleIndex];
 
     if(currentArticle.hasGallery == true){
         var gallery;
@@ -28,4 +31,7 @@
         gallery = $("<img>").attr("src", currentArticle.sources);
         $(".article__img").append(gallery);
     }
+
+    utility.populateArticleDetails(articleContent, articleIndex);
+
 }(window, window.THUNDERSTORM, window.jQuery));
