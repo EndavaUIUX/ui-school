af31260858e4a21dc9dc0738e97d528ddee7abd5
IuliaCojocaru <iulia.cojocaru@endava.com>
2015-07-31 09:11:50 +0300

    changes

--- assets/js/UI/article-content.js
+++ assets/js/UI/article-content.js
@@ -7,39 +7,42 @@
 
     THUNDERSTORM.modules.articles.init({
         sourceName : 'articles',
-        shouldGenerate : false
+        shouldGenerate : false,
+        callback: populateArticlePage
     });
 
-    var pageUrl = window.location.href,
-        articleUrlNumber = pageUrl.split("#")[1],
-        article = THUNDERSTORM.modules.articles.data['articles'][articleUrlNumber],
-        currentArticle = THUNDERSTORM.modules.utility.imageSourceGenerator(article),
-        viewMoreButton = $("<div></div>").html("View gallery").addClass("button__gallery"),
-        icoViewMore = $('<i></i>'),
-        utility = THUNDERSTORM.modules.utility,
-        articleContent = $(".article__body"),
-        titleContainer = $('.title'),
-        infoAuthor = $('.article-informations__author'),
-        infoDate = $('.article-informations__date'),
-        elementsObject = {titleContainer : titleContainer, infoAuthor : infoAuthor, infoDate : infoDate};
+    function populateArticlePage(){
+        var pageUrl = window.location.href,
+            articleUrlNumber = pageUrl.split("#")[1],
+            article = THUNDERSTORM.modules.articles.data['articles'][articleUrlNumber],
+            currentArticle = THUNDERSTORM.modules.utility.imageSourceGenerator(article),
+            viewMoreButton = $("<div></div>").html("View gallery").addClass("button__gallery"),
+            icoViewMore = $('<i></i>'),
+            utility = THUNDERSTORM.modules.utility,
+            articleContent = $(".article__body"),
+            titleContainer = $('.title'),
+            infoAuthor = $('.article-informations__author'),
+            infoDate = $('.article-informations__date'),
+            elementsObject = {titleContainer : titleContainer, infoAuthor : infoAuthor, infoDate : infoDate};
         viewMoreButton.append(icoViewMore);
 
-    if(currentArticle.hasGallery == true){
-        var gallery;
-        currentArticle.sources.filter(function(item, index){
-            gallery = $("<img>").attr("src", item);
+        if(currentArticle.hasGallery == true){
+            var gallery;
+            currentArticle.sources.filter(function(item, index){
+                gallery = $("<img>").attr("src", item);
+                $(".article__gallery").append(gallery);
+                if(index != 0) gallery.addClass("hidden");
+            });
+            $(".article__gallery").addClass("hasGallery");
+            $(".article__gallery").append(viewMoreButton);
+
+        } else {
+            gallery = $("<img>").attr("src", currentArticle.sources);
             $(".article__gallery").append(gallery);
-            if(index != 0) gallery.addClass("hidden");
-        });
-        $(".article__gallery").addClass("hasGallery");
-        $(".article__gallery").append(viewMoreButton);
+        }
 
-    } else {
-        gallery = $("<img>").attr("src", currentArticle.sources);
-        $(".article__gallery").append(gallery);
+        utility.populateArticleTitle(elementsObject, article);
+        utility.populateArticleDetails(article, articleContent);
     }
-
-    utility.populateArticleTitle(elementsObject, article);
-    utility.populateArticleDetails(article, articleContent);
 
 }(window, window.THUNDERSTORM, window.jQuery));
