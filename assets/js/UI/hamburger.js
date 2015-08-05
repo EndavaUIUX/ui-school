217e12a9cb0a540c6ffc82c38ace2cf6618fca7d
Alexandru Bolboceanu <you@example.com>
2015-08-05 14:12:24 +0300

    -abolbocean =Left Navigation Menu=

---
+++ assets/js/UI/hamburger.js
@@ -0,0 +1,10 @@
+
+$('#menuHamburger').click(function () {    
+        if ($('input#menuHamburger').is(':checked')) {
+            $('nav.menu-left').addClass("show-menu-left");
+        }else{
+            $('nav.menu-left').removeClass("show-menu-left");
+        }
+
+}); 
+
