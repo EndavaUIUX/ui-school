ee344afedcc046c917955f102d210f5d1443d5fe
IuliaCojocaru <iulia.cojocaru@endava.com>
2015-08-05 18:03:14 +0300

    right menu page

--- assets/js/UI/hamburger.js
+++ assets/js/UI/hamburger.js
@@ -1,10 +1,8 @@
-
-$('#menuHamburger').click(function () {    
-        if ($('input#menuHamburger').is(':checked')) {
-            $('nav.menu-left').addClass("show-menu-left");
-        }else{
-            $('nav.menu-left').removeClass("show-menu-left");
-        }
-
+$('#menuHamburger').click(function () {
+    if ($('input#menuHamburger').is(':checked')) {
+        $('nav.menu-left').addClass("show-menu-left");
+    } else{
+        $('nav.menu-left').removeClass("show-menu-left");
+    }
 }); 
 
