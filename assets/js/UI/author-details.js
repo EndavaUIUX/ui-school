0761528f2d895b246481582bab3e491206bde067
IuliaCojocaru <iulia.cojocaru@endava.com>
2015-08-06 17:47:45 +0300

    remove input checkbox

--- assets/js/UI/author-details.js
+++ assets/js/UI/author-details.js
@@ -1,13 +1,15 @@
 /**
  * Created by icojocaru on 8/5/2015.
  */
-$('#userProfile').click(function () {
+$('.profile').click(function () {
     $('.title').html("Most Recent Articles");
-    if ($('input#userProfile').is(':checked')) {
+    if (!$('.menu-right').hasClass("show-menu-right")) {
         $('.menu-right').addClass("show-menu-right");
         $('.rightminidiv').addClass("hide-user-icon");
-    } else{
+    }
+
+    $('.user').click(function (){
         $('.menu-right').removeClass("show-menu-right");
         $('.rightminidiv').removeClass("hide-user-icon");
-    }
+    });
 });
