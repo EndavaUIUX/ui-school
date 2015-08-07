7af7345fb7718e9d571daf9cace0b52253f90aea
Tatiana <starciuc.tatiana@gmail.com>
2015-08-07 10:38:42 +0300

    Close right and left menu on click on body

--- assets/js/UI/author-details.js
+++ assets/js/UI/author-details.js
@@ -1,12 +1,34 @@
 /**
  * Created by icojocaru on 8/5/2015.
  */
-$('#userProfile').click(function () {
-    if ($('input#userProfile').is(':checked')) {
-        $('.menu-right').addClass("show-menu-right");
-        $('.rightminidiv').addClass("hide-user-icon");
-    } else{
-        $('.menu-right').removeClass("show-menu-right");
-        $('.rightminidiv').removeClass("hide-user-icon");
-    }
-});
+(function() {
+    'use strict';
+    var $menuRight = $('.menu-right'),
+        $rightMinDiv = $('.rightminidiv'),
+        $profile = $('.profile'),
+        $user = $('.user'),
+        $title = $('.title');
+
+    $profile.click(function (e) {
+
+        $title.html("Most Recent Articles");
+        if (!$menuRight.hasClass("show-menu-right")) {
+            $menuRight.addClass("show-menu-right");
+            $rightMinDiv.addClass("hide-user-icon");
+        }
+    });
+
+    $user.click(function () {
+        $menuRight.removeClass("show-menu-right");
+        $rightMinDiv.removeClass("hide-user-icon");
+    });
+
+    $(document).on("click", function (e) {
+        var targetEl = $(e.target);
+
+        if (!targetEl.is('.profile') && !targetEl.is('.menu-right') && !targetEl.is('.article-recent h2')) {
+            $('.menu-right').removeClass("show-menu-right");
+            $('.rightminidiv').removeClass("hide-user-icon");
+        }
+    });
+}());
