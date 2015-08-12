6aa371f1f501b0958596f10069570a90a1590d1a
Tatiana <starciuc.tatiana@gmail.com>
2015-08-12 11:13:00 +0300

    menu functionality

--- assets/js/UI/author-details.js
+++ assets/js/UI/author-details.js
@@ -6,44 +6,33 @@
     var $menuRight = $('.menu-right'),
         $rightMinDiv = $('.rightminidiv'),
         $profile = $('.profile'),
-        $user = $('.user'),
+        $hideMenu = $('.user, .right-menu-backdrop'),
         $title = $('.title-recent'),
-        $rightMenuBackdrop = $('.right-menu-backdrop'),
-        $leftthird = $('.leftthird');
+        $rightMenuBackdrop = $('.right-menu-backdrop');
 
     $profile.click(function (e) {
-        console.log("aaa",e.target);
+        e.preventDefault();
         e.stopPropagation();
+
+        var  $menuLeft = $('nav.menu-left'),
+            $leftMenuBackdrop = $('.left-menu-backdrop');
+
         $title.html("Most Recent Articles");
         if (!$menuRight.hasClass("show-menu-right")) {
             $menuRight.addClass("show-menu-right");
             $rightMenuBackdrop.addClass("show");
-            $leftthird.addClass("hideHamburger");
+
+            $menuLeft.removeClass("show-menu-left");
+            $leftMenuBackdrop.removeClass("show");
         }
     });
 
-    $user.click(function (e) {
-        console.log("bbb",e.target);
+    $hideMenu.click(function (e) {
+        e.preventDefault();
         e.stopPropagation();
         $menuRight.removeClass("show-menu-right");
         $rightMinDiv.removeClass("hide-user-icon");
         $rightMenuBackdrop.removeClass("show");
-        $leftthird.removeClass("hideHamburger");
     });
-
-   /* $('body').on("click tap touchend", function (e) {
-        var targetEl = $(e.target);
-        console.log("ccc",e.target);
-
-        if (!targetEl.is('.profile')
-            && !targetEl.is('.menu-right')
-            && !targetEl.is('.article-recent h2')
-            && !targetEl.is('.menu')) {
-
-            $menuRight.removeClass("show-menu-right");
-            $rightMenuBackdrop.removeClass("show");
-            $leftthird.removeClass("hideHamburger");
-        }
-    });*/
 }());
 
