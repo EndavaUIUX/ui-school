d10ddf1dfd9fb8a3a6234e74d16c91aa73d11706
Iuliana Ciobanu <iuliana92is@yahoo.com>
2015-08-12 09:59:54 +0300

    iuliana2 something

--- assets/js/modules/slider.js
+++ assets/js/modules/slider.js
@@ -94,3 +94,58 @@
     
     
 }(window.jQuery))
+
+    /* ==========================================================================
+     swipe function                                                            
+     ========================================================================== */
+
+      var swipeFunction = {
+        touches: {
+            "touchstart": {"x": -1, "y": -1},
+            "touchmove": {"x": -1, "y": -1},
+            "touchend": false,
+            "direction": "undetermined"
+        },
+        touchHandler: function (event) {
+            var touch;
+            if (typeof event !== 'undefined') {
+                event.preventDefault();
+                if (typeof event.touches !== 'undefined') {
+                    touch = event.touches[0];
+                    // console.log(event.type);
+                    switch (event.type) {
+                        case 'touchstart':
+                        case 'touchmove':
+                            swipeFunction.touches[event.type].x = touch.pageX;
+                            swipeFunction.touches[event.type].y = touch.pageY;
+                            break;
+                        case 'touchend':
+                            // console.log('touchend');
+                            swipeFunction.touches[event.type] = true;
+                            if (swipeFunction.touches.touchstart.x > -1 && swipeFunction.touches.touchmove.x > -1) {
+                                swipeFunction.touches.direction = swipeFunction.touches.touchstart.x < swipeFunction.touches.touchmove.x ? "right" : "left";
+
+                                // Continue here for previous and next button
+                                // alert(touches.direction);
+                                console.log(swipeFunction.touches.direction);
+                            }
+                        default:
+                            
+                    }
+                }
+            }
+        },
+        init: function () {
+            console.log('init');
+            var image = document.querySelector('.slider__slide img');
+            image.addEventListener('touchstart', swipeFunction.touchHandler, false);
+            image.addEventListener('touchmove', swipeFunction.touchHandler, false);
+            image.addEventListener('touchend', swipeFunction.touchHandler, false);
+        }
+    };
+
+    console.log(swipeFunction);
+    console.log(swipeFunction.init);
+    swipeFunction.init();
+
+    utility.sortLatestArticlesAccessed(recentArticles);
